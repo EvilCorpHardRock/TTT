5ef39932ef6e13055d05330d4869fa82dbd49d14
Nikita <kna300103@gmail.com>
2020-01-22 21:26:51 +0400

    123

---
+++ Assets/enemy_movement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy_movement : MonoBehaviour
+{
+    Rigidbody2D m_Rigidbody2D;
+    public float speed = 1;
+    public Transform Target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+            Vector2 touchposition = Target.position;
+
+
+            touchposition.x = touchposition.x - m_Rigidbody2D.position.x;
+            touchposition.y = touchposition.y - m_Rigidbody2D.position.y;
+
+            touchposition = touchposition.normalized * speed;
+
+
+            m_Rigidbody2D.velocity = touchposition;
+
+
+
+    }
+}
---
+++ Assets/move.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    Transform t;
+    public Transform m;
+    // Start is called before the first frame update
+    void Start()
+    {
+        t = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        t.position = m.position;
+    }
+}
